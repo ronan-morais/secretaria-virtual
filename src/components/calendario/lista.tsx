ad1267fc2eb3233b07cba37b3629840efc51366d
Ronan Morais <61247547+nanza86@users.noreply.github.com>
2023-03-14 01:36:12 -0300

    calendar buttons

--- src/components/calendario/lista.tsx
+++ src/components/calendario/lista.tsx
@@ -89,25 +89,29 @@ export function Lista({
             </b>
           </div>
           <div className="flex flex-row items-center gap-5 text-begeEscuro">
-            <div className="flex flex-row gap-1">
+            <div className="flex flex-row gap-1 items-center">
               <HiOutlineClock className="w-5 h-5 text-begeMedio" /> Horario:{" "}
               <b>{horaInicio}</b>
             </div>
-            <div className="flex flex-row gap-1">
+            <div className="flex flex-row gap-1 items-center">
               <TbHanger className="w-5 h-5 text-begeMedio" /> Farda:{" "}
               <b>{farda}</b>
             </div>
           </div>
           <div className="flex flex-row items-start gap-1  text-begeEscuro">
-            <HiOutlineCalendar className="w-5 h-5 text-begeMedio" /> Data:{" "}
-            <b>{hinario}</b>
+            <HiOutlineCalendar className="w-5 h-5 text-begeMedio" />{" "}
+            <span className="pt-1 sm:pt-0">
+              Hinário:
+              <b>{hinario}</b>{" "}
+            </span>
           </div>
           <div className="pt-3 flex w-full md:justify-start gap-3">
             <button
               type="button"
               className="bg-begeEscuro font-bold text-white p-3 py-2 rounded-lg w-full md:w-auto flex flex-row items-center justify-center gap-1"
             >
-              <HiOutlineInformationCircle className="w-6 h-6" /> Mais informações
+              <HiOutlineInformationCircle className="w-6 h-6" /> Mais
+              informações
             </button>
           </div>
         </div>
