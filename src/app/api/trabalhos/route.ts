8541cb9743757e76a6dfe3dccca1a22e79973934
Ronan Morais <ronan86@gmail.com>
2023-04-06 15:38:46 -0300

    db prisma

--- src/app/api/trabalhos/route.ts
+++ src/app/api/trabalhos/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { prisma } from '@/db';
 
 export async function GET() {
 
   const trabalhos = await prisma.trabalhos.findMany()
-
+    
   return NextResponse.json({ trabalhos })
 }
